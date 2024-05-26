cfca30f4366f6c95a5f83f366bffd629aa45cb7e
Amonoo <benjaminamonoowilberforce@outlook.com>
2024-05-26 19:44:20 +0000

    change background color of the view component in App.js to 'rgb(250,0,250)'

--- App.js
+++ App.js
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'rgb(250,0,250)',
     alignItems: 'center',
     justifyContent: 'center',
   },
